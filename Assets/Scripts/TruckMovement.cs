c298606909cdf7aa93c943aa27dd5c73658c6560
Neelesh <neeldudeepic990@gmail.com>
2021-01-18 18:18:25 -0600

    Created TruckMovement script
    Created TruckMovement script that handles all truck movement

---
+++ Assets/Scripts/TruckMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TruckMovement : MonoBehaviour
+{
+    public float movementSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementSpeed = Random.Range(movementSpeed, movementSpeed + 30);
+        transform.GetChild(0).GetComponent<Rigidbody>().drag = Random.Range(0.1f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * movementSpeed * Time.deltaTime;
+    }
+}
